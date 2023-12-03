3d6ec6f65a8c4abb396e41c8f0ba32a81a8bea66
Devangam Sajja Rajesh <104376917+Rajeshds20@users.noreply.github.com>
2023-12-03 14:58:58 +0530

    Updated Bucket UI

--- frontend/src/pages/Bucket.js
+++ frontend/src/pages/Bucket.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Typography, Button, AppBar, Toolbar, IconButton, TextField, Tooltip } from '@mui/material';
+import { Container, Typography, Button, AppBar, Toolbar, IconButton, TextField, Tooltip, Paper } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { BsClipboard } from 'react-icons/bs';
 
 const BucketsPage = () => {
@@ -44,13 +44,28 @@ const BucketsPage = () => {
     }, []);
 
 
+    function isUrlOrEmail(input) {
+        const urlRegex = /^(https?:\/\/)?([\w-]+(\.[\w-]+)+\/?)\S*$/;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (urlRegex.test(input)) {
+            return 'url';
+        } else if (emailRegex.test(input)) {
+            return 'email';
+        } else {
+            return 'text';
+        }
+    }
+
+
+
     const handleInputChange = (e) => {
         setNewBucket({ ...newBucket, [e.target.name]: e.target.value })
     }
 
     const handleCopyCode = (copyText) => {
         navigator.clipboard.writeText(copyText);
-        alert('Code Copied');
+        alert('Text Copied');
     };
 
     const handleCodeChange = (e) => {
@@ -113,40 +128,86 @@ const BucketsPage = () => {
 
             {/* Add 2 features, creating a bucket and generating code, and retriving a bucket based on code */}
             {codeBucket ? (
-                <Container maxWidth="md" sx={{ textAlign: 'center', marginTop: '2rem' }}>
-                    <Typography
-                        variant="outlined"
-                        fullWidth
-                        sx={{
-                            marginBottom: '1rem',
-                            cursor: 'pointer',
-                            color: 'blue',
-                            fontSize: '2rem',
-                        }}
-                        onClick={() => { handleCopyCode(codeBucket?.code) }}
-                    >
-                        {codeBucket?.code}
-                    </Typography>
-                    <Tooltip title="Copy Code" arrow>
-                        <IconButton onClick={() => { handleCopyCode(codeBucket?.code) }} color="primary">
-                            <BsClipboard size={30} />
-                        </IconButton>
-                    </Tooltip>
-                    <Typography variant="h4" gutterBottom>
-                        Bucket
-                    </Typography>
-                    <Typography variant="h6" gutterBottom>
-                        {codeBucket?.title}
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
-                        {codeBucket?.content}
-                    </Typography>
-                    <Tooltip title="Copy Content" arrow>
-                        <IconButton onClick={() => { handleCopyCode(codeBucket?.content) }} color="primary">
-                            <BsClipboard size={30} />
-                        </IconButton>
-                    </Tooltip>
+                <Container maxWidth="md" style={{ textAlign: 'center', marginTop: '2rem' }}>
+                    {/* Code Bucket */}
+                    <Paper elevation={3} style={{ padding: '2rem', marginBottom: '2rem', borderRadius: '8px' }}>
+                        <Typography
+                            variant="h4"
+                            gutterBottom
+                            style={{ marginBottom: '1rem', color: '#1e90ff', fontSize: '2rem', fontWeight: 'bold' }}
+                        >
+                            Code
+                        </Typography>
+                        <Typography
+                            variant="outlined"
+                            fullWidth
+                            style={{ cursor: 'pointer', color: '#1e90ff', fontSize: '2rem', wordBreak: 'break-all' }}
+                            onClick={() => handleCopyCode(codeBucket?.code)}
+                        >
+                            {codeBucket?.code}
+                        </Typography>
+                        <Tooltip title="Copy Code" arrow>
+                            <IconButton onClick={() => handleCopyCode(codeBucket?.code)} color="black" style={{ marginTop: '-15px' }}>
+                                <BsClipboard size={25} />
+                            </IconButton>
+                        </Tooltip>
+                    </Paper>
 
+                    {/* Bucket Details */}
+                    <Paper elevation={3} style={{ padding: '2rem', marginBottom: '2rem', borderRadius: '8px' }}>
+                        <Typography variant="h4" gutterBottom style={{ color: '#1e90ff', fontSize: '1.8rem', fontWeight: 'bold' }}>
+                            Bucket Details
+                        </Typography>
+                        <Typography variant="h3" gutterBottom style={{ marginTop: '1rem' }}>
+                            {codeBucket?.title}
+                        </Typography>
+                        {/* <Typography variant="h4" gutterBottom style={{ marginTop: '1rem' }}>
+                            {codeBucket?.content}
+                        </Typography> */}
+                        {isUrlOrEmail(codeBucket?.content) === 'url' ? (
+                            <Link style={{
+                                textDecoration: 'none',
+                                color: '#1e90ff',
+                                fontSize: '1.5rem',
+                                wordBreak: 'break-all',
+                            }} href={codeBucket?.content} target="_blank" rel="noopener noreferrer">
+                                {codeBucket?.content}
+                            </Link>
+                        ) : isUrlOrEmail(codeBucket?.content) === 'email' ? (
+                            <a style={{
+                                textDecoration: 'none',
+                                color: '#1e90ff',
+                                fontSize: '1.5rem',
+                                wordBreak: 'break-all',
+                            }} href={`mailto:${codeBucket?.content}`}>{codeBucket?.content}</a>
+                        ) : (
+                            <div style={{
+                                // Style as a input but disabled
+                                border: '1px solid #1e90ff',
+                                borderRadius: '8px',
+                                padding: '1rem',
+                                marginTop: '1rem',
+                                color: '#1e90ff',
+                                fontSize: '1.5rem',
+                                wordBreak: 'break-all',
+                                backgroundColor: 'rgb(218, 203, 203)',
+                                cursor: 'default'
+                            }}>
+                                <Typography variant="h4" gutterBottom style={{ marginTop: '1rem' }}>
+                                    {codeBucket?.content}
+                                </Typography>
+                            </div>
+                        )}
+                        <Tooltip title="Copy Content" arrow>
+                            <IconButton
+                                onClick={() => handleCopyCode(codeBucket?.content)}
+                                color="primary"
+                                style={{ marginTop: '0px' }}
+                            >
+                                <BsClipboard size={25} />
+                            </IconButton>
+                        </Tooltip>
+                    </Paper>
                 </Container>
             ) : (
                 <Container maxWidth="md">
@@ -204,9 +265,10 @@ const BucketsPage = () => {
                         </Button>
                     </div>
                 </Container>
-            )}
+            )
+            }
 
-        </div>
+        </div >
     );
 };
 
